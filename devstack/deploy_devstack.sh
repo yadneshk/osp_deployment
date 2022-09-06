02010af188e25e711c44f886cc80c67e7c39c3a4
Yadnesh Kulkarni <ykulkarn@redhat.com>
2022-09-06 14:41:25 +0530

    Update deploy_devstack.sh

--- devstack/deploy_devstack.sh
+++ devstack/deploy_devstack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo useradd -s /bin/bash -d /opt/stack -m stack
+sudo useradd -s /bin/bash -d /home/stack -m stack
 sudo chmod +x /opt/stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
 sudo yum install git wget tmux -y
