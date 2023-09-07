b77e5a8c8982fd556c9345d9e5fc7a45fc8650c9
Yadnesh Kulkarni <24690448+yadneshk@users.noreply.github.com>
2023-09-07 10:27:12 +0530

    Update deploy_devstack.sh

--- devstack/deploy_devstack.sh
+++ devstack/deploy_devstack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo useradd -s /bin/bash -d /opt/stack -m stack
+sudo chmod +x /opt/stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
 sudo su - stack
 sudo yum install git wget tmux vim -y
