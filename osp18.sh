dd1855002be5185a0385de301055e6c6213c7fc8
Yadnesh Kulkarni <24690448+yadneshk@users.noreply.github.com>
2025-01-02 23:03:00 +0530

    Update osp18.sh

--- osp18.sh
+++ osp18.sh
@@ -1,32 +1,66 @@
 #!/bin/bash
 
-export DNS=
-export NTP=
+NNCP_TIMEOUT=480
+set -euxo pipefail
 
 cd ~/install_yamls/devsetup
-make edpm_play_cleanup
-make edpm_compute_cleanup
-
-cd ~/install_yamls/
-make openstack_deploy_cleanup
-make openstack_cleanup
-make crc_storage_cleanup
-
-cd ~/install_yamls/devsetup
-make crc_cleanup
-CPUS=8 MEMORY=20480 DISK=60 make crc
+CPUS=36 MEMORY=96000 DISK=100 make crc 
 make download_tools
 
-cd ~/install_yamls/
-NETWORK_ISOLATION=false make openstack
+make crc_attach_default_interface
+
+EDPM_TOTAL_NODES=2 make edpm_compute
+
+cd ..
 make crc_storage
 make input
-NETWORK_ISOLATION=false make openstack_deploy
 
-make ansibleee
-cd ~/install_yamls/devsetup
-make crc_attach_default_interface
-DATAPLANE_DNS_SERVER=$DNS DATAPLANE_CHRONY_NTP_SERVER=$NTP EDPM_COMPUTE_SUFFIX=0 DATAPLANE_SINGLE_NODE=false make edpm_compute
-DATAPLANE_DNS_SERVER=$DNS DATAPLANE_CHRONY_NTP_SERVER=$NTP EDPM_COMPUTE_SUFFIX=0 DATAPLANE_SINGLE_NODE=false make edpm_compute_repos
-cd ~/install_yamls/
-DATAPLANE_DNS_SERVER=$DNS DATAPLANE_CHRONY_NTP_SERVER=$NTP EDPM_COMPUTE_SUFFIX=0 DATAPLANE_SINGLE_NODE=false make edpm_deploy
+make openstack
+sleep 180 
+
+make openstack_deploy
+
+while true; do
+    output=$(oc get oscp openstack-galera-network-isolation | grep galera)
+    
+    if echo "$output" | grep -q "True"; then
+        echo "openstack-galera-network-isolation Ready"
+        break  # Exit the loop if the string is found, remove this if you want to keep checking
+    else
+        echo "Waiting for oscp to be ready..."
+    fi  
+    sleep 5  # Wait 5 seconds before checking again, adjust the time as needed
+done
+
+DATAPLANE_TOTAL_NODES=2 make edpm_deploy
+while true; do
+    output=$(oc get osdpns | grep edpm)
+    
+    if echo "$output" | grep -q "True"; then
+        echo "openstack-edpm-ipam Ready"
+        break  # Exit the loop if the string is found, remove this if you want to keep checking
+    else
+        echo "Waiting for osdpns to be ready..."
+    fi  
+    sleep 5  # Wait 5 seconds before checking again, adjust the time as needed
+done
+
+make edpm_nova_discover_hosts
+
+oc -n openstack rsh openstackclient openstack compute service list
+
+oc create -f - <<EOF
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: cluster-observability-operator
+  namespace: openshift-operators
+spec:
+  channel: development
+  installPlanApproval: Automatic
+  name: cluster-observability-operator
+  source: redhat-operators
+  sourceNamespace: openshift-marketplace
+EOF
+
+cd ~/install_yamls/devsetup; make edpm_deploy_instance
